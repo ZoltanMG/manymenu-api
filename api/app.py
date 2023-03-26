1c85cd50ce17b5bb52631a3c3d6c7d2635c54ee6
ZoltanMG <zoltanmoragarcia@gmail.com>
2023-03-25 22:25:26 -0500

    se ajusta el nombre de algunas variables

--- api/app.py
+++ api/app.py
@@ -7,33 +7,28 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'], strict_slashes=False)
 def home():
-    return storage.get_all(response=True)
+    all_recipes_to_response = storage.get_all(response=True)
+    return all_recipes_to_response
 
 
 @app.route("/reseta", methods=['POST', 'DELETE'], strict_slashes=False)
-def resetas():
+def reseta():
     if request.method == "POST":
-        res_recipe = request.json
+        recipe_name = request.json['recipe_name']
+        ingredients = request.json['ingredients']
         new_recipe = Recipe(
-            recipe_name=res_recipe['recipe_name'],
-            ingredients=res_recipe['ingredients']
+            recipe_name=recipe_name,
+            ingredients=ingredients
             )
         new_recipe.save()
         return jsonify(new_recipe.__dict__), 200
     if request.method == "DELETE":
-        print("delete:", request.json)
-
-        recipe = storage.get(request.json['id'])
-        recipe.delete()
+        recipe_to_delete = storage.get(request.json['id'])
+        recipe_to_delete.delete()
         return jsonify({"delete":"Ok"}), 203
 
 @app.after_request
 def after(response):
-    """
-    Esta función verifica la sesión después de una solicitud y
-    le da al CORS con la interfaz que permite la conexión
-    entre la parte posterior y la frontal.
-    """
     response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
